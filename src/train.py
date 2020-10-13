2954b40dd918305a2cbda35acec2d7295ccb4e50
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2020-10-14 09:44:54 +1100

    include function comments in training script

--- src/train.py
+++ src/train.py
@@ -11,14 +11,19 @@ from keras.optimizers import Adam
 
 
 def build_2d_model(filters, kernels, layers):
+	"""Build a model to play minesweeper!"""
 	
+	# Here, we're taking a 3D input in the form of a 2D grid with multiple channels
+	# The channels are my way of representing discrete numbers to the model (how many nearby cells are mined?)
 	inputs = Input(shape=(GRID_R, GRID_C, CHANNELS))
 
+	# Start by projecting into a different 3D space so we can start using residual connections right away
 	x = Conv2D(filters=filters, kernel_size=kernels, padding='same')(inputs)
 	x = BatchNormalization()(x)
 	x = Activation('relu')(x)
 	x = Dropout(0.2)(x)
 	
+	# Do this over and over... the Add() is our residual connection
 	for _ in range(layers-1):
 		y = Conv2D(filters=filters, kernel_size=kernels, padding='same')(x)
 		y = BatchNormalization()(y)
@@ -26,6 +31,9 @@ def build_2d_model(filters, kernels, layers):
 		y = Dropout(0.2)(y)
 		x = Add()([x, y])
 
+	# Project into a space that "squeezes" to 2D and apply a sigmoid to map from 0 to 1
+	# This 0 to 1 mapping works well with the binary_crossentropy loss function, because...
+	# We're predicting which cells are safe and that's more of a multi-out regression than a classification
 	outputs = Conv2D(filters=1, kernel_size=(1, 1), padding='same', activation='sigmoid')(x)
 
 	model = Model(inputs=inputs, outputs=outputs)
@@ -34,6 +42,8 @@ def build_2d_model(filters, kernels, layers):
 
 
 def compact_frame_to_convolutional_features(string, shape):
+	"""Convert a puzzle from its string representation to a numpy array of features"""
+
 	rows, cols = shape
 
 	features = np.zeros((rows, cols, CHANNELS), dtype=np.uint8)
@@ -47,6 +57,8 @@ def compact_frame_to_convolutional_features(string, shape):
 
 
 def compact_frame_to_convolutional_labels(string, shape):
+	"""Convert a puzzle from its string representation to a numpy array of labels"""
+
 	rows, cols = shape
 
 	labels = np.zeros((rows, cols, 1), dtype=np.uint8)
@@ -60,6 +72,8 @@ def compact_frame_to_convolutional_labels(string, shape):
 
 
 def frames_to_dataset(dataset, shape):
+	"""Convert puzzle strings into state-action pairs for training a model"""
+
 	features = np.zeros((len(dataset), shape[0], shape[1], CHANNELS), dtype=np.uint8)
 	labels = np.zeros((len(dataset), shape[0], shape[1], 1), dtype=np.uint8)
 
@@ -71,6 +85,8 @@ def frames_to_dataset(dataset, shape):
 
 
 def train_model_from_file(training_datafile, validation_datafile, model, shape):
+	"""Load training and validation data from designated files and train a model to predict safe moves"""
+
 	training_dataset = [x for x in open(training_datafile, 'r').read().split('\n') if len(x) == shape[0]*shape[1]]
 	print(len(training_dataset), 'items loaded into training dataset')
 
