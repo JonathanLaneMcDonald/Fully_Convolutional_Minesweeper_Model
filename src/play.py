2f7cdf80e4369a61f10ceaf6e43cf30b09c8f5c2
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2020-10-14 14:54:54 +1100

    formatting updates to play script

--- src/play.py
+++ src/play.py
@@ -1,26 +1,27 @@
 
 from common import *
-from evaluate import board_to_features, generate_heat_map, select_top_moves
+from evaluate import generate_heat_map, select_top_moves
 from MinesweeperClass import *
 
-import numpy as np
 from keras.models import load_model
 
 from tkinter import *
 
 SQ = 24
 
-h='0123456789abcdef'
-def itoh(n):
-	return h[(n>>20)&0xf]+h[(n>>16)&0xf]+h[(n>>12)&0xf]+h[(n>>8)&0xf]+h[(n>>4)&0xf]+h[(n>>0)&0xf]
 
-class display( Frame ):
+def itoh(n):
+	h = '0123456789abcdef'
+	return h[(n >> 20) & 0xf] + h[(n >> 16) & 0xf] + h[(n >> 12) & 0xf] + h[(n >> 8) & 0xf] + h[(n >> 4) & 0xf] + h[(n >> 0) & 0xf]
+
+
+class MinesweeperUI(Frame):
 
 	def draw(self):
 		self.canvas.delete('all')
 		
 		prediction = None
-		if self.model != None and self.use_model:
+		if self.model and self.use_model:
 			prediction = generate_heat_map(self.game, self.model)
 
 		mine_field = self.game.get_mine_field()
@@ -38,31 +39,31 @@ class display( Frame ):
 				fill = 'white'
 				outline = 'black'
 				if flag_field[r][c]:
-					self.canvas.create_rectangle(1+c*SQ,1+r*SQ,1+(c+1)*SQ,1+(r+1)*SQ,fill='yellow',outline=outline)
+					self.canvas.create_rectangle(1+c*SQ, 1+r*SQ, 1+(c+1)*SQ, 1+(r+1)*SQ, fill='yellow', outline=outline)
 					fill = 'yellow'
 				elif visi_field[r][c]:
 					if mine_field[r][c]:
-						self.canvas.create_rectangle(1+c*SQ,1+r*SQ,1+(c+1)*SQ,1+(r+1)*SQ,fill='red',outline=outline)
+						self.canvas.create_rectangle(1+c*SQ, 1+r*SQ, 1+(c+1)*SQ, 1+(r+1)*SQ, fill='red', outline=outline)
 					else:
-						self.canvas.create_rectangle(1+c*SQ,1+r*SQ,1+(c+1)*SQ,1+(r+1)*SQ,fill='white',outline=outline)
+						self.canvas.create_rectangle(1+c*SQ, 1+r*SQ, 1+(c+1)*SQ, 1+(r+1)*SQ, fill='white', outline=outline)
 						if prox_field[r][c]:
-							self.canvas.create_text(c*SQ+SQ/2,r*SQ+SQ/2,font=font,text=str(prox_field[r][c]))
+							self.canvas.create_text(c*SQ+SQ/2, r*SQ+SQ/2, font=font, text=str(prox_field[r][c]))
 				else:
-					self.canvas.create_rectangle(1+c*SQ,1+r*SQ,1+(c+1)*SQ,1+(r+1)*SQ,fill='grey',outline=outline)
+					self.canvas.create_rectangle(1+c*SQ, 1+r*SQ, 1+(c+1)*SQ, 1+(r+1)*SQ, fill='grey', outline=outline)
 					fill = 'grey'
 
 				# these cells are 'border cells'
 				if bord_field[r][c]:
-					self.canvas.create_rectangle(2+c*SQ,2+r*SQ,(c+1)*SQ,(r+1)*SQ,fill=fill,outline='yellow')
+					self.canvas.create_rectangle(2+c*SQ, 2+r*SQ, (c+1)*SQ, (r+1)*SQ, fill=fill, outline='yellow')
 
 				# this is where the 'cursor' is
 				if r == self.row and c == self.col:
-					self.canvas.create_rectangle(2+c*SQ,2+r*SQ,(c+1)*SQ,(r+1)*SQ,fill=fill,outline='red')
+					self.canvas.create_rectangle(2+c*SQ, 2+r*SQ, (c+1)*SQ, (r+1)*SQ, fill=fill, outline='red')
 					if visi_field[r][c]:
 						if mine_field[r][c]:
-							self.canvas.create_rectangle(1+c*SQ,1+r*SQ,1+(c+1)*SQ,1+(r+1)*SQ,fill='red',outline=outline)
+							self.canvas.create_rectangle(1+c*SQ, 1+r*SQ, 1+(c+1)*SQ, 1+(r+1)*SQ, fill='red', outline=outline)
 						elif prox_field[r][c]:
-							self.canvas.create_text(c*SQ+SQ/2,r*SQ+SQ/2,font=font,text=str(prox_field[r][c]))
+							self.canvas.create_text(c*SQ+SQ/2, r*SQ+SQ/2, font=font, text=str(prox_field[r][c]))
 
 		if prediction:
 			# find the minimum and maximum extents of the prediction values - this way, we can assign percentage values to cells
@@ -75,11 +76,9 @@ class display( Frame ):
 					sum_over_interesting.append(p[0])
 
 			if len(sum_over_interesting):
-				#print sum_over_interesting
 				coldest = min(sum_over_interesting)
 				warmest = max(sum_over_interesting)
 				t_range = warmest - coldest
-				average = float(t_range)/2
 
 				visualize = []
 				if t_range:
@@ -91,7 +90,7 @@ class display( Frame ):
 						if coldest <= p[0] <= warmest:
 							value = float(p[0] - coldest) / t_range
 
-						visualize.append((value,(r,c)))
+						visualize.append((value, (r, c)))
 
 				for v in visualize:
 					value = 2*v[0] - 1
@@ -106,37 +105,31 @@ class display( Frame ):
 					else:
 						blue -= int(128*value)
 						green -= int(128*value)
-					
-					if red < 0:		red = 0
-					if green < 0:	green = 0
-					if blue < 0:	blue = 0	
 
-					color = (red<<16) + (green<<8) + blue
+					red = max(0, red)
+					green = max(0, green)
+					blue = max(0, blue)
+
+					color = (red << 16) + (green << 8) + blue
 
 					r = v[1][0]
 					c = v[1][1]
 
 					if visi_field[r][c]:
-						self.canvas.create_oval(3+c*SQ,3+r*SQ,c*SQ+SQ/3,r*SQ+SQ/3,fill='white')
+						self.canvas.create_oval(3+c*SQ, 3+r*SQ, c*SQ+SQ/3, r*SQ+SQ/3, fill='white')
 					else:
-						self.canvas.create_oval(3+c*SQ,3+r*SQ,c*SQ+SQ/3,r*SQ+SQ/3,fill='#'+itoh(color))
-
-						#if self.game.get_game_status() == self.game.INPROGRESS:
-						#	write_value = abs(value)
-
-						#	self.canvas.create_text(43+c*SQ,13+r*SQ,text=str(write_value)[:5])
+						self.canvas.create_oval(3+c*SQ, 3+r*SQ, c*SQ+SQ/3, r*SQ+SQ/3, fill='#'+itoh(color))
 
 					if r == self.row and c == self.col:
-						print ('Model Predicts:',value)
+						print('Model Predicts:', value)
 
 		if self.showing_mines:
 			for r in range(game_rows):
 				for c in range(game_cols):
 					if mine_field[r][c]:
-						self.canvas.create_oval(c*SQ+SQ/4,r*SQ+SQ/4,c*SQ+3*SQ/4,r*SQ+3*SQ/4,fill='red')
+						self.canvas.create_oval(c*SQ+SQ/4, r*SQ+SQ/4, c*SQ+3*SQ/4, r*SQ+3*SQ/4, fill='red')
 
 		self.canvas.update_idletasks()
-		#time.sleep(0.2)
 
 	def nnsolver(self):
 		if self.use_model:
@@ -155,30 +148,47 @@ class display( Frame ):
 
 						self.game.visit_cell(selected_move)
 					else:
-						self.game.visit_cell(safe_moves[int(npr()*len(safe_moves))])
+						generous_first_moves = self.game.get_generous_first_moves()
+						if len(generous_first_moves):
+							self.game.visit_cell(generous_first_moves[int(npr()*len(generous_first_moves))])
+						else:
+							self.game.visit_cell(safe_moves[int(npr()*len(safe_moves))])
 				else:
-					self.game.forfeit_game()# because we're surrounded by mines ;(
+					self.game.forfeit_game()
 
 				self.draw()
 
 	def keyboard(self, event):
-		if event.keysym == 'Escape':		self.quit()
+		if event.keysym == 'Escape':
+			self.quit()
 
-		if event.keysym == 'Up':			self.row -= 1
-		if event.keysym == 'Down':			self.row += 1
-		if event.keysym == 'Left':			self.col -= 1
-		if event.keysym == 'Right':			self.col += 1
+		if event.keysym == 'Up':
+			self.row -= 1
+		if event.keysym == 'Down':
+			self.row += 1
+		if event.keysym == 'Left':
+			self.col -= 1
+		if event.keysym == 'Right':
+			self.col += 1
 
-		if self.row < 0:					self.row = 0
-		if self.row >= self.rows:			self.row = self.rows-1
-		if self.col < 0:					self.col = 0
-		if self.col >= self.cols:			self.col = self.cols-1
+		if self.row < 0:
+			self.row = 0
+		if self.row >= self.rows:
+			self.row = self.rows-1
+		if self.col < 0:
+			self.col = 0
+		if self.col >= self.cols:
+			self.col = self.cols-1
 
-		if event.char == 'e':				self.mines += 1
-		if event.char == 'q':				self.mines -= 1
-		if event.char == 'e' or event.char == 'q':	print ('Mines:',self.mines)
+		if event.char == 'e':
+			self.mines += 1
+		if event.char == 'q':
+			self.mines -= 1
+		if event.char == 'e' or event.char == 'q':
+			print('Mines:', self.mines)
 		
-		if event.char == 'i':				self.use_model ^= 1
+		if event.char == 'i':
+			self.use_model ^= 1
 
 		if event.keysym == 'g':
 			self.game = Minesweeper(self.rows, self.cols, self.mines)
@@ -187,7 +197,7 @@ class display( Frame ):
 			self.showing_mines ^= 1
 
 		message = ''
-		if self.game: # don't do game actions when there's no game or you'll break things
+		if self.game:
 			if event.keysym == 's':
 				self.nnsolver()
 
@@ -219,20 +229,20 @@ class display( Frame ):
 			
 			if self.game.game_status == self.game.VICTORY:
 				for i in range(10):
-					print ('yay, victory!')
+					print('yay, victory!')
 			elif self.game.game_status == self.game.DEFEAT:
 				for i in range(10):
-					print ('boo, you lose')
+					print('boo, you lose')
 
-		print (event.keysym, event.char, message)
+		print(event.keysym, event.char, message)
 
 		self.draw()
 	
 	def __init__(self):
 		Frame.__init__(self)
 		self.master.title('Lane\'s MineSweeper')
-		self.master.rowconfigure(0,weight=1)
-		self.master.columnconfigure(0,weight=1)
+		self.master.rowconfigure(0, weight=1)
+		self.master.columnconfigure(0, weight=1)
 		self.grid(sticky=N+S+E+W)
 
 		self.showing_mines = 0
@@ -241,11 +251,12 @@ class display( Frame ):
 		self.rows = GRID_R
 		self.cols = GRID_C
 		
-		self.canvas=Canvas(self,width=self.cols*SQ+1, height=self.rows*SQ+1, bg='white')
-		self.canvas.grid(row=0,column=0)
+		self.canvas = Canvas(self, width=self.cols*SQ+1, height=self.rows*SQ+1, bg='white')
+		self.canvas.grid(row=0, column=0)
 
 		self.game = Minesweeper(self.rows, self.cols, self.mines)
-		self.model = load_model('debug model 16x16x40 1')
+		self.model = load_model('minesweeper model 8x8x10')
+		self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 		self.use_model = 0
 
 		self.row = 0
@@ -255,4 +266,5 @@ class display( Frame ):
 
 		self.draw()
 
-display().mainloop()
+
+MinesweeperUI().mainloop()
