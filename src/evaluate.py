3c5e76a29fed80113006b2213327183c38d2032f
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2020-10-14 13:29:20 +1100

    formatting and updated a few variable names

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,3 @@
-
 from common import *
 from MinesweeperClass import *
 
@@ -6,6 +5,7 @@ import numpy as np
 from math import log
 
 from keras.models import load_model
+
 
 def board_to_features(game):
 	rows = game.rows
@@ -24,29 +24,32 @@ def board_to_features(game):
 
 	return new_array
 
+
 def generate_heat_map(game, model):
-	input_features = np.array(board_to_features(game), dtype=np.intc).reshape(1, GRID_R, GRID_C, CHANNELS)
-	prediction = model.predict(input_features).reshape(GRID_CELLS)
+	model_prediction = model.predict(np.array(board_to_features(game)).reshape(1, GRID_R, GRID_C, CHANNELS))
+
+	prediction = model_prediction.reshape(GRID_CELLS)
 
 	flag_field = game.get_flagged_field()
 
 	heat_map = []
 	for i in range(len(prediction)):
-		r = i//GRID_C
-		c = i%GRID_C
+		r = i // GRID_C
+		c = i % GRID_C
 		if flag_field[r][c]:
 			# FIXME: don't change this value here.  check for flags somewhere else because this compromises the goal of the function
-			heat_map.append((float(-1),(r,c)))
+			heat_map.append((float(-1), (r, c)))
 		else:
-			heat_map.append((float(prediction[i]),(r,c)))
+			heat_map.append((float(prediction[i]), (r, c)))
 
 	return heat_map
+
 
 def select_top_moves(game, model, moves_requested):
 	heat_map = generate_heat_map(game, model)
 	heat_map.sort()
 	heat_map.reverse()
-	
+
 	# just start by picking the top preferences
 	top_moves = []
 	possible_moves = game.get_visible_field()
@@ -54,33 +57,35 @@ def select_top_moves(game, model, moves_requested):
 		r = i[1][0]
 		c = i[1][1]
 		if possible_moves[r][c] == 0:
-			top_moves.append((r,c))
+			top_moves.append((r, c))
 			if len(top_moves) == moves_requested:
 				return top_moves
 	return top_moves
 
-def build_difficulty_stats(R, C, G):
-	for m in range(MIN_MINES,MAX_MINES+1):
+
+def build_difficulty_stats(rows, cols, games):
+	for mines in range(MIN_MINES, MAX_MINES + 1):
 		wins = 0
 		losses = 0
-		for i in range(G):
-			game = Minesweeper( R, C, m )
+		for i in range(games):
+			game = Minesweeper(rows, cols, mines)
 
 			while game.get_game_status() == game.INPROGRESS:
 				unvisited_cells = game.get_moves()
-				selection = int(npr()*len(unvisited_cells))
-				
+				selection = int(npr() * len(unvisited_cells))
+
 				r = unvisited_cells[selection][0]
 				c = unvisited_cells[selection][1]
 
-				game.visit_cell((r,c))
+				game.visit_cell((r, c))
 
 			if game.get_game_status() == game.VICTORY:
 				wins += 1
 			elif game.get_game_status() == game.DEFEAT:
 				losses += 1
-			
-		print (m,'mines',wins,'wins',losses,'losses')
+
+		print(mines, 'mines', wins, 'wins', losses, 'losses')
+
 
 def evaluate(target_games, model, mines=MIN_MINES, nn_predicts_opening_move=False):
 	deaths = 0
@@ -89,7 +94,7 @@ def evaluate(target_games, model, mines=MIN_MINES, nn_predicts_opening_move=Fals
 	games_won = 0
 	global_likelihood = 0
 	while games_played < target_games:
-		game = Minesweeper( GRID_R, GRID_C, mines )
+		game = Minesweeper(GRID_R, GRID_C, mines)
 		games_played += 1
 
 		likelihood = 1.0
@@ -112,20 +117,28 @@ def evaluate(target_games, model, mines=MIN_MINES, nn_predicts_opening_move=Fals
 				else:
 					generous_first_moves = game.get_generous_first_moves()
 					if len(generous_first_moves):
-						game.visit_cell(generous_first_moves[int(npr()*len(generous_first_moves))])
+						game.visit_cell(generous_first_moves[int(npr() * len(generous_first_moves))])
 					else:
-						game.visit_cell(safe_moves[int(npr()*len(safe_moves))])
+						game.visit_cell(safe_moves[int(npr() * len(safe_moves))])
 			else:
-				game.forfeit_game()# because we're surrounded by mines ;(
+				game.forfeit_game()  # because we're surrounded by mines ;(
 
-		global_likelihood += log(likelihood)/log(10)
+		global_likelihood += log(likelihood) / log(10)
 		if not to_smitherines:
 			games_won += 1
 
 		if games_played % 1 == 0:
-			print (games_played, float(deaths)/moves_played, float(games_won)/games_played, float(moves_played)/games_played, float(global_likelihood)/games_played)
+			print(
+				games_played, float(deaths) / moves_played, float(games_won) / games_played,
+				float(moves_played) / games_played, float(global_likelihood) / games_played)
 
-	return float(deaths)/moves_played, float(games_won)/games_played, float(moves_played)/games_played, float(global_likelihood)/games_played
+	return float(deaths) / moves_played, float(games_won) / games_played, float(moves_played) / games_played, float(
+		global_likelihood) / games_played
+
 
 if __name__ == "__main__":
-	evaluate(10000, load_model('debug model 16x16x40 1'))
+	model = load_model('minesweeper model 8x8x10')
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model.summary()
+
+	evaluate(10000, model)
